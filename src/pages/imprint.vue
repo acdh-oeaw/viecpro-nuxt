071104137602b9e57ae02e1b16e37d6510c90291
Stefan Probst <stefanprobst@posteo.de>
2023-04-25 20:46:23 +0200

    feat: add imprint page

--- src/pages/imprint.vue
+++ src/pages/imprint.vue
@@ -1,20 +1,31 @@
 <script lang="ts" setup>
+import { request } from "@stefanprobst/request";
+
 import MainContent from "@/components/main-content.vue";
 import PageTitle from "@/components/page-title.vue";
-import { definePageMeta, useI18n } from "#imports";
+import { definePageMeta, useAsyncData, useI18n } from "#imports";
 import { type Locale, type Schema } from "~/config/i18n.config";
+import { createImprintUrl } from "~/config/imprint.config";
 
 definePageMeta({
 	title: "pages.imprint.title",
 });
 
-const { t } = useI18n<Schema, Locale>();
+const { locale, t } = useI18n<Schema, Locale>();
+
+const imprint = await useAsyncData("pages.imprint", () => {
+	const url = createImprintUrl(locale.value);
+
+	return request(url, { responseType: "text" });
+});
 </script>
 
 <template>
 	<MainContent>
 		<div class="mx-auto w-full max-w-content px-4 py-2">
 			<PageTitle>{{ t("pages.imprint.title") }}</PageTitle>
+			<!-- eslint-disable-next-line vue/no-v-html -->
+			<div class="prose" v-html="imprint.data.value" />
 		</div>
 	</MainContent>
 </template>
