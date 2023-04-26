b851d40772df5b09e4b6532fdfda2845d1212326
Stefan Probst <stefanprobst@posteo.de>
2023-04-26 11:27:19 +0200

    feat: add error page

---
+++ src/error.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import MainContent from "@/components/main-content.vue";
+import PageTitle from "@/components/page-title.vue";
+import { clearError, definePageMeta, useI18n } from "#imports";
+import { type Locale, type Schema } from "~/config/i18n.config";
+
+definePageMeta({
+	title: "pages.error.title",
+});
+
+defineProps<{
+	error: Error;
+}>();
+
+function handleError() {
+	clearError({ redirect: "/" });
+}
+
+const { t } = useI18n<Schema, Locale>();
+</script>
+
+<template>
+	<MainContent class="h-full">
+		<div class="mx-auto h-full w-full max-w-content px-4 py-2">
+			<PageTitle>{{ t("pages.error.title") }}</PageTitle>
+			<div class="grid h-full place-items-center">
+				<button @click="handleError">{{ t("common.reset") }}</button>
+			</div>
+		</div>
+	</MainContent>
+</template>
