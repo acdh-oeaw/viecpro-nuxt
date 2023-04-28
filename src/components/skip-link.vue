d453f8a8d501ba922b4e3a761224642a72986ff6
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-28 13:59:59 +0200

    feat: add skip to main content link

---
+++ src/components/skip-link.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { NuxtLink } from "#components";
+
+const id = "main-content";
+const hash = "#" + id;
+</script>
+
+<template>
+	<NuxtLink
+		class="fixed translate-y-[-120%] rounded bg-white px-4 py-3 text-black focus:translate-y-0"
+		:href="{ hash }"
+	>
+		Skip to main content
+	</NuxtLink>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import AppFooter from "@/components/app-footer.vue";
 import AppHeader from "@/components/app-header.vue";
+import SkipLink from "@/components/skip-link.vue";
 </script>
 
 <template>
 	<div class="grid min-h-full grid-rows-[auto_1fr_auto]">
+		<SkipLink />
 		<AppHeader />
 		<slot />
 		<AppFooter />
