1f2268f04d88fff9fb6e3ffe2952734f8410d493
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-28 13:17:39 +0200

    chore: adds dummy pages

---
+++ src/pages/documentation.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import MainContent from "@/components/main-content.vue";
+</script>
+
+<template>
+	<MainContent>
+		<div>About Page</div>
+	</MainContent>
+</template>
---
+++ src/pages/search.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import MainContent from "@/components/main-content.vue";
+</script>
+
+<template>
+	<MainContent>
+		<div>Database Page</div>
+	</MainContent>
+</template>
