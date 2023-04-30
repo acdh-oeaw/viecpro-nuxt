9c1262f6249ed25563fafbe628eb1de22ac3e841
Stefan Probst <stefanprobst@posteo.de>
2023-04-30 11:01:22 +0200

    fix: ensure skiplink moves keyboard focus to main

--- src/components/skip-link.vue
+++ src/components/skip-link.vue
@@ -6,12 +6,21 @@ const { t } = useI18n();
 
 const id = "main-content";
 const hash = "#" + id;
+
+/**
+ * @see https://bugzilla.mozilla.org/show_bug.cgi?id=308064
+ */
+function onClick() {
+	const element = document.getElementById(id);
+	element?.focus();
+}
 </script>
 
 <template>
 	<NuxtLink
 		class="fixed -translate-y-full rounded bg-white px-4 py-3 transition focus:translate-y-0"
 		:href="{ hash }"
+		@click="onClick"
 	>
 		{{ t("common.skip-to-main-content") }}
 	</NuxtLink>
