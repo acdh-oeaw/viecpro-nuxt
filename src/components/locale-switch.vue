398001330b82295c394301f015b35b6ef5767913
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-07-27 13:40:53 +0200

    fix [locale-switch.vue]: resolves #4 (#19)
    - removes watchEffect in locale-switch
    - adds computed property instead
    - resolves #4

--- src/components/locale-switch.vue
+++ src/components/locale-switch.vue
@@ -1,25 +1,24 @@
 <script lang="ts" setup>
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/vue";
-import { type Ref } from "vue";
-import { ref, watchEffect } from "vue";
+import { computed } from "vue";
 
-// TODO: GP: resolve these imports with correct typing, see also note in i18n.config
 import { useI18n } from "@/composables/use-i18n";
-import { type LocaleObject, locales } from "~/config/i18n.config";
+import { locales } from "~/config/i18n.config";
 
 const { locale, setLocale } = useI18n();
-const selectedLocale: Ref<LocaleObject> = ref(locales[locale.value]);
 
-watchEffect(() => {
-	setLocale(selectedLocale.value.code);
+const selectedLocale = computed(() => {
+	return locales[locale.value];
 });
 </script>
 
 <template>
 	<div class="mx-4 rounded bg-white px-4 py-2 text-black">
-		<Listbox v-model="selectedLocale">
+		<Listbox
+			:model-value="selectedLocale"
+			@update:model-value="(selectedValue) => setLocale(selectedValue.code)"
+		>
 			<ListboxButton>{{ selectedLocale.code }}</ListboxButton>
-
 			<ListboxOptions class="fixed mt-4 -translate-x-4 rounded bg-white px-4 py-2 shadow-lg">
 				<ListboxOption
 					v-for="loc in locales"
