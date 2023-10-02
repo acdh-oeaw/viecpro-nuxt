20ab19f91d9d9240f521f5246b1c9e2bf470241c
Robin Kaggl <48437174+kaggl@users.noreply.github.com>
2023-10-02 15:22:53 +0200

    Feat  tooltip component (#21)
    Co-authored-by: kaggl <robin.kaggl@oeaw.ac.at>

--- src/pages/documentation/[...slug].vue
+++ src/pages/documentation/[...slug].vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ChevronRightIcon } from "@heroicons/vue/24/solid";
+import { ChevronRightIcon } from "@heroicons/vue/24/outline";
 
 import MainContent from "@/components/main-content.vue";
 import { useI18n } from "@/composables/use-i18n";
