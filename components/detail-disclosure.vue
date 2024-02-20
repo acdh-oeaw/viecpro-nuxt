35ab7f4231c58a8785b8bfd3a2eba06526bad19e
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2024-02-20 16:07:52 +0100

    added pagination do detail disclosure

--- components/detail-disclosure.vue
+++ components/detail-disclosure.vue
@@ -6,7 +6,7 @@ import GenericDisclosure from "@/components/generic-disclosure.vue";
 import type { AnyEntity } from "@/types/schema";
 import { NuxtLink } from "#components";
 
-defineProps<{
+const props = defineProps<{
 	rels: Array<AnyEntity>;
 	gridClass?: string;
 	title: string;
@@ -19,6 +19,10 @@ defineProps<{
 
 const locale = useLocale();
 const t = useTranslations();
+
+const page = ref(0);
+const limit = ref(10);
+const all = props.rels.length;
 </script>
 
 <template>
@@ -29,6 +33,51 @@ const t = useTranslations();
 	>
 		<slot>
 			<div class="p-2">
+				<div v-if="all > limit" class="mb-4 flex justify-between">
+					<button v-if="page > 1" class="rounded border p-1" @click="page--">
+						<ChevronRight class="h-5 w-5 rotate-180" />
+					</button>
+					<div v-else class="flex items-center rounded border p-1 text-gray-300">
+						<ChevronRight class="h-5 w-5 rotate-180" />
+					</div>
+					<div>
+						<div class="flex flex-wrap justify-center gap-1">
+							<button v-if="page > 2" class="underline" @click="page = 0">1</button>
+
+							<span v-if="page > 3">...</span>
+							<template v-for="n in 5" :key="n - 2 + page">
+								<button
+									v-if="n - 2 + page >= 1 && n - 2 + page <= Math.ceil(all / limit)"
+									:class="n === 3 ? 'font-semibold' : 'underline'"
+									@click="page = n - 3 + page"
+								>
+									{{ n - 2 + page }}
+								</button>
+							</template>
+							<span v-if="page + 4 < Math.ceil(all / limit)">...</span>
+							<button
+								v-if="page + 3 < Math.ceil(all / limit)"
+								class="underline"
+								@click="page = Math.ceil(all / limit) - 1"
+							>
+								{{ Math.ceil(all / limit) }}
+							</button>
+						</div>
+						<div class="text-center">
+							{{ page * limit + 1 }} - {{ Math.min((page + 1) * limit, all) }} / {{ all }}
+						</div>
+					</div>
+					<button
+						v-if="page < Math.ceil(all / limit) - 1"
+						class="rounded border p-1"
+						@click="page++"
+					>
+						<ChevronRight class="h-5 w-5" />
+					</button>
+					<div v-else class="flex items-center rounded border p-1 text-gray-300">
+						<ChevronRight class="h-5 w-5" />
+					</div>
+				</div>
 				<div class="grid gap-2" :class="linkTo ? 'mr-6 ' + gridClass : gridClass">
 					<template v-for="header in headers" :key="header">
 						<span v-if="typeof header === 'string'">
@@ -39,7 +88,11 @@ const t = useTranslations();
 						</span>
 					</template>
 				</div>
-				<div v-for="hit in rels" :key="String(hit)" class="mt-1 border-t pt-1">
+				<div
+					v-for="hit in rels.slice(page * limit, (page + 1) * limit)"
+					:key="String(hit)"
+					class="mt-1 border-t pt-1"
+				>
 					<component
 						:is="linkTo ? NuxtLink : 'div'"
 						:to="`/${locale}/detail/${hit?.target?.model.toLowerCase() + 's'}/${
