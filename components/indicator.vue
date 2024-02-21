a84bd7f63874d1f850a910327e04ce55a838406e
kaggl <robin.kaggl@oeaw.ac.at>
2024-02-21 17:32:57 +0100

    resolves #69

--- components/indicator.vue
+++ components/indicator.vue
@@ -6,11 +6,14 @@ defineProps<{
 
 <template>
 	<div class="flex gap-1 rounded border p-2">
-		<div :class="status === 'red' && 'bg-red-500'" class="h-6 w-6 rounded-full bg-slate-300" />
+		<div :class="status === 'red' ? 'bg-red-500' : 'bg-slate-300'" class="h-6 w-6 rounded-full" />
 		<div
-			:class="status === 'yellow' && 'bg-yellow-500'"
-			class="h-6 w-6 rounded-full bg-slate-300"
+			:class="status === 'yellow' ? 'bg-yellow-500' : 'bg-slate-300'"
+			class="h-6 w-6 rounded-full"
 		/>
-		<div :class="status === 'green' && 'bg-green-500'" class="h-6 w-6 rounded-full bg-slate-300" />
+		<div
+			:class="status === 'green' ? 'bg-green-500' : 'bg-slate-300'"
+			class="h-6 w-6 rounded-full"
+		/>
 	</div>
 </template>
