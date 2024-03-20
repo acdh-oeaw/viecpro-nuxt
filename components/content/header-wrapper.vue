4a6e98641f8f0d006834e5fe7d3c29598f23c9b0
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2024-03-20 15:37:54 +0100

    added button which links to detail page of hierarchy node, resolves #90

--- components/content/header-wrapper.vue
+++ components/content/header-wrapper.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="mx-auto w-fit">
 		<div
-			class="mx-2 mb-8 flex w-fit flex-col justify-center truncate hyphens-auto border-b-4 pb-4 text-center text-3xl text-primary-600 sm:text-5xl lg:flex-row"
+			class="mx-2 mb-8 flex w-fit flex-col justify-center hyphens-auto border-b-4 pb-4 text-center text-3xl text-primary-600 sm:text-5xl lg:flex-row"
 		>
 			<slot />
 		</div>
