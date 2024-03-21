0b4affe37241744d2ea1a80c0c7e2ddaff4acd30
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2024-03-21 15:19:14 +0100

    new disclosures

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,6 +62,21 @@ const config: Config = {
 				container: "80rem",
 				content: "64rem",
 			},
+			keyframes: {
+				slideDown: {
+					from: { height: "0px" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				slideUp: {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0px" },
+				},
+			},
+			animation: {
+				slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+				slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+			},
+
 			typography: {
 				DEFAULT: {
 					css: {
