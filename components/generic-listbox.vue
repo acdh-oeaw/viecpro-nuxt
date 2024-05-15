f77f7a59dd1c8ada8216721f9c6f573410693449
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2024-05-15 13:20:56 +0200

    added sort by selection to detail disclosures

--- components/generic-listbox.vue
+++ components/generic-listbox.vue
@@ -31,7 +31,16 @@ defineEmits(["change"]);
 		class="relative"
 		@update:model-value="(to) => $emit('change', to)"
 	>
-		<Float portal as="div" class="relative">
+		<Float
+			enter="transition duration-100 ease-out"
+			enter-from="transform scale-95 -translate-y-8 opacity-0"
+			enter-to="transform scale-100 translate-y-0 opacity-100"
+			leave="transition duration-75 ease-in"
+			leave-from="transform scale-100 opacity-100"
+			leave-to="transform scale-95 opacity-0"
+			portal
+			adaptive-width
+		>
 			<div class="flex w-full items-center gap-4">
 				<ListboxLabel v-if="$slots.label">
 					<slot name="label" />
@@ -45,23 +54,21 @@ defineEmits(["change"]);
 					<ChevronsUpDown class="h-5 w-5 text-gray-500" />
 				</ListboxButton>
 			</div>
-			<MenuTransition>
-				<ListboxOptions
-					v-if="items.length != 0"
-					as="div"
-					class="absolute z-10 mt-2 flex w-max min-w-full flex-col divide-y rounded border bg-white shadow-xl"
-					:class="$slots.label && 'ml-10'"
+			<ListboxOptions
+				v-if="items.length != 0"
+				as="div"
+				class="absolute z-10 mt-2 flex w-full min-w-max flex-col divide-y rounded border bg-white shadow-xl"
+				:class="$slots.label && 'ml-10'"
+			>
+				<ListboxOption
+					v-for="item in items"
+					:key="item.value"
+					class="cursor-pointer list-none p-2 first-of-type:rounded-t last-of-type:rounded-b ui-selected:bg-primary-300 ui-active:bg-primary-300"
+					:value="item"
 				>
-					<ListboxOption
-						v-for="item in items"
-						:key="item.value"
-						class="cursor-pointer list-none p-2 first-of-type:rounded-t last-of-type:rounded-b ui-selected:bg-primary-300 ui-active:bg-primary-300"
-						:value="item"
-					>
-						<span>{{ item.label }}</span>
-					</ListboxOption>
-				</ListboxOptions>
-			</MenuTransition>
+					<span>{{ item.label }}</span>
+				</ListboxOption>
+			</ListboxOptions>
 		</Float>
 	</Listbox>
 </template>
