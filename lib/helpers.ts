f3023a08d3e9ec5a58c15140573581aee3c9909b
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2024-06-24 14:00:18 +0200

    added link symbol to links in sources

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -11,5 +11,8 @@ export function downloadAsJson(content: object, title: string) {
 export function detectURLsAddLinks(content: string) {
 	const urlRegex =
 		/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-	return content.replace(urlRegex, '<a class="underline" href="$1" target="_blank">$1</a>');
+	return content.replace(
+		urlRegex,
+		'<a style="text-decoration: underline; font-weight: 600;" href="$1" target="_blank">$1&#8599;</a>',
+	);
 }
