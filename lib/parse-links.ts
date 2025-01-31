87173c6a1b4ee2e76ae7f9ee10eadc18dda5d801
Stefan Probst <stefanprobst@posteo.de>
2025-01-31 21:03:55 +0100

    fix: make parsed links visible

--- lib/parse-links.ts
+++ lib/parse-links.ts
@@ -1,10 +1,10 @@
-const regex = /https?:\/\/[\w.]+/g;
+const regex = /https?:\/\/[\w./]+/g;
 
 /**
  * Parse links into html anchor elements, handling any trailing punctuation marks.
  */
 export function parseLinks(input: string): string {
 	return input.replace(regex, (url: string) => {
-		return `<a href="${url}">${url}</a>`;
+		return `<a class="underline hover:no-underline" href="${url}">${url}</a>`;
 	});
 }
