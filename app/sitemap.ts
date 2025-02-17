24e20c919342fd93eab1cbce09564b1a714a39cc
Stefan Probst <stefanprobst@posteo.de>
2025-02-17 23:26:01 +0100

    feat: add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ import { glob } from "fast-glob";
 import type { MetadataRoute } from "next";
 
 import { env } from "@/config/env.config";
-import { locales } from "@/config/i18n.config";
+import { client } from "@/lib/typesense/client";
 
 const baseUrl = env.NEXT_PUBLIC_APP_BASE_URL;
 
@@ -41,17 +41,59 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 		routes.push(segments.join("/"));
 	});
 
-	const entries = locales.flatMap(() => {
-		return routes.map((pathname) => {
-			return {
-				url: String(createUrl({ baseUrl, pathname })),
-				/**
-				 * Only add `lastmod` when the publication date is actually known.
-				 * Don't use the build date instead.
-				 */
-				// lastModified: new Date(),
-			};
-		});
+	const courts = await client
+		.collections([env.NEXT_PUBLIC_TYPESENSE_COLLECTION_PREFIX, "court"].join(""))
+		.documents()
+		.export({ include_fields: "id" });
+
+	for (const line of courts.split("\n")) {
+		if (line === "") continue;
+		const entry = JSON.parse(line) as { id: string };
+		routes.push(`/courts/${entry.id}`);
+	}
+
+	const institutions = await client
+		.collections([env.NEXT_PUBLIC_TYPESENSE_COLLECTION_PREFIX, "institution"].join(""))
+		.documents()
+		.export({ include_fields: "id" });
+
+	for (const line of institutions.split("\n")) {
+		if (line === "") continue;
+		const entry = JSON.parse(line) as { id: string };
+		routes.push(`/institutions/${entry.id}`);
+	}
+
+	const persons = await client
+		.collections([env.NEXT_PUBLIC_TYPESENSE_COLLECTION_PREFIX, "person"].join(""))
+		.documents()
+		.export({ include_fields: "id" });
+
+	for (const line of persons.split("\n")) {
+		if (line === "") continue;
+		const entry = JSON.parse(line) as { id: string };
+		routes.push(`/persons/${entry.id}`);
+	}
+
+	const places = await client
+		.collections([env.NEXT_PUBLIC_TYPESENSE_COLLECTION_PREFIX, "place"].join(""))
+		.documents()
+		.export({ include_fields: "id" });
+
+	for (const line of places.split("\n")) {
+		if (line === "") continue;
+		const entry = JSON.parse(line) as { id: string };
+		routes.push(`/places/${entry.id}`);
+	}
+
+	const entries = routes.map((pathname) => {
+		return {
+			url: String(createUrl({ baseUrl, pathname })),
+			/**
+			 * Only add `lastmod` when the publication date is actually known.
+			 * Don't use the build date instead.
+			 */
+			// lastModified: new Date(),
+		};
 	});
 
 	return entries;
